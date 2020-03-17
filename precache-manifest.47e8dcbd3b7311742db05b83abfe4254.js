4e6fbd60ee0357d4801ea8b5b0809bb5edaa2bb0
nguyen.hong.hai <nguyen.hong.hai@framgia.com>
2020-03-17 08:28:29 +0700

    Updates

---
+++ precache-manifest.47e8dcbd3b7311742db05b83abfe4254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e749e5440bdf37bf9a085b578f68e0b",
+    "url": "/game-caro/index.html"
+  },
+  {
+    "revision": "53140c21e66f215cd420",
+    "url": "/game-caro/static/css/main.a795e376.chunk.css"
+  },
+  {
+    "revision": "5bd34afc94edc8f04d79",
+    "url": "/game-caro/static/js/2.374d6514.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/game-caro/static/js/2.374d6514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53140c21e66f215cd420",
+    "url": "/game-caro/static/js/main.018c173e.chunk.js"
+  },
+  {
+    "revision": "0497a30cbb9b9f76d3c0",
+    "url": "/game-caro/static/js/runtime-main.3dc6786a.js"
+  }
+]);
